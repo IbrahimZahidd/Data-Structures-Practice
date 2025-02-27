f7518bb288a14bc18ff1d9dca783c887757f44c9
IbrahimZahidd <98962434+IbrahimZahidd@users.noreply.github.com>
2025-02-27 17:08:07 +0500

    Updating selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+void selectionSort(int arr[], int n) {
+    for(int i=0; i<n-1;i++) {
+        int smallestIndex=i;
+        for(int j=i+1;j<n;j++) {
+            if(arr[j]<arr[smallestIndex]) {
+                smallestIndex=j;
+            }
+        }
+        swap(arr[i], arr[smallestIndex]);
+    }
+}
+
+int main() {
+    int size = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+    selectionSort(arr, size);
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
