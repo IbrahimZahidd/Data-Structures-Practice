009fb68773213e06bb4f489e3c61520467c808ad
IbrahimZahidd <98962434+IbrahimZahidd@users.noreply.github.com>
2025-02-27 15:12:39 +0500

    Creating bubbleSortCode.cpp

---
+++ bubbleSortCode.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool isSwap=false;
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) { // Compare adjacent elements
+                swap(arr[j], arr[j+1]); // Swap if they are in the wrong order
+                isSwap=true;
+            }
+        }
+        if(isSwap == false) {
+            return;
+        }
+    }
+}
+
+int main() {
+    int size = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+    bubbleSort(arr, size);
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
