22f9de48331fbb056e1987352e665f7d7e500ebf
IbrahimZahidd <98962434+IbrahimZahidd@users.noreply.github.com>
2025-02-27 17:37:18 +0500

    Creating insertionSort.cpp

---
+++ insertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for(int i=1; i<n;i++) {
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]>curr) {
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1]=curr;
+    }
+}
+
+int main() {
+    int size = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+    insertionSort(arr, size);
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
